f451409ce9f716bbb9c99f66b433555d7008d3b4
imba-tjd <109224573@qq.com>
2020-07-20 11:13:53 +0800

    Revert "Revert "从 HttpUtility.UrlEncode 迁移到 Uri.EscapeDataString""
    This reverts commit 409b5402ec455117716bbdf8d374f3ea0272a7ef.

--- TranslatorLibrary/TencentFYJTranslator.cs
+++ TranslatorLibrary/TencentFYJTranslator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Web;
 using System.Text.Json;
 
 namespace TranslatorLibrary
@@ -41,7 +40,7 @@ namespace TranslatorLibrary
                 .Append("&nonce_str=").Append(salt)
                 .Append("&source=").Append(srcLang)
                 .Append("&target=").Append(desLang)
-                .Append("&text=").Append(HttpUtility.UrlEncode(q).ToUpper())
+                .Append("&text=").Append(Uri.EscapeDataString(q).ToUpper())
                 .Append("&time_stamp=").Append(CommonFunction.GetTimeStamp());
             sb.Append("&sign=" + CommonFunction.EncryptString(sb.ToString() + "&app_key=" + appKey).ToUpper());
             string req = sb.ToString();
