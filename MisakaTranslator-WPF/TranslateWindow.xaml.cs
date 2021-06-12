28249dc7370dca8b6e2ce7f33c431c0e72626ad4
imba-tjd <109224573@qq.com>
2021-06-12 16:40:02 +0800

    调整Hook翻译事件async方式

--- MisakaTranslator-WPF/TranslateWindow.xaml.cs
+++ MisakaTranslator-WPF/TranslateWindow.xaml.cs
@@ -409,10 +409,8 @@ namespace MisakaTranslator_WPF
         /// <summary>
         /// Hook模式下调用的事件
         /// </summary>
-        public void DataRecvEventHandler(object sender, SolvedDataRecvEventArgs e)
+        public async void DataRecvEventHandler(object sender, SolvedDataRecvEventArgs e)
         {
-            Application.Current.Dispatcher.BeginInvoke((Action)(async () =>
-            {
 
                 //1.得到原句
                 string source = e.Data.Data;
@@ -435,7 +433,7 @@ namespace MisakaTranslator_WPF
                 if (repairedText.Length != 0 && repairedText.Length <= Common.appSettings.TransLimitNums)
                 {
                     //2.5 清除面板
-                    SourceTextPanel.Children.Clear();
+                    Application.Current.Dispatcher.Invoke(() => SourceTextPanel.Children.Clear());
 
                     _currentsrcText = repairedText;
 
@@ -449,13 +447,13 @@ namespace MisakaTranslator_WPF
                     {
                         transRes1 = await _translator1.TranslateAsync(beforeString, Common.UsingDstLang, Common.UsingSrcLang);
                         if(transRes1 == null)
-                            Growl.ErrorGlobal(_translator1.GetType().Name + ": " + _translator1.GetLastError());
+                            Application.Current.Dispatcher.Invoke(() => Growl.ErrorGlobal(_translator1.GetType().Name + ": " + _translator1.GetLastError()));
                     }
                     if (_translator2 != null)
                     {
                         transRes2 = await _translator2.TranslateAsync(beforeString, Common.UsingDstLang, Common.UsingSrcLang);
                         if(transRes2 == null)
-                            Growl.ErrorGlobal(_translator2.GetType().Name + ": " + _translator2.GetLastError());
+                            Application.Current.Dispatcher.Invoke(() => Growl.ErrorGlobal(_translator2.GetType().Name + ": " + _translator2.GetLastError()));
                     }
 
                     //6.翻译后处理
@@ -463,8 +461,10 @@ namespace MisakaTranslator_WPF
                     string afterString2 = _afterTransHandle.AutoHandle(transRes2);
 
                     //7.翻译结果显示到窗口上
+                    Application.Current.Dispatcher.Invoke(() => {
                     FirstTransText.Text = afterString1;
                     SecondTransText.Text = afterString2;
+                    });
 
                     //8.翻译结果记录到队列
                     if (_gameTextHistory.Count > 5)
@@ -486,10 +486,6 @@ namespace MisakaTranslator_WPF
                         }
                     }
                 }
-
-            }));
-
-
         }
 
         private void ChangeSize_Item_Click(object sender, RoutedEventArgs e)
